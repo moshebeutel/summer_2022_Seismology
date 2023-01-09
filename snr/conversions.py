f1d8a6d26854dd9e4e061ccfcfdb6a4984def38f
moshe <moshebeutel@gmail.com>
2023-01-09 07:27:40 +0100

    Limit search by caller with insane factor to prevent infinite searches

--- snr/conversions.py
+++ snr/conversions.py
@@ -1,19 +1,21 @@
 import torch
 
 
-def snr_to_factor(trace, label, clean_snr, desired_snr, noise_trace, calc_snr, precision=0.0099):
+def snr_to_factor(trace, label, clean_snr, desired_snr, noise_trace, calc_snr, precision=0.0099,
+                  insane_factor=100000000):
     assert desired_snr <= int(clean_snr), 'desired_snr should be less than the clean snr'
-    label = int(label)
     factor = 0
     diff = 100
-    fix_step = 100.0
+    fix_step = 100000.0
     positive_diff = True
-    while abs(diff) > precision:
+    while abs(diff) > precision and factor < insane_factor:
         # while (factor + fix_step) <= 0 or factor + fix_step >= int(clean_snr):
         #     fix_step /= 2.0
         factor += fix_step
-        trace_added_noise = torch.clone(trace) + (factor * noise_trace)
+        # print('factor', factor)
+        trace_added_noise = torch.clone(trace + (factor * noise_trace))
         calculated_snr = calc_snr(trace_added_noise[0].numpy(), label)
+        # print('calculated_snr', calculated_snr)
         diff = calculated_snr - desired_snr
         new_positive_diff = diff > 0
 
@@ -22,3 +24,4 @@ def snr_to_factor(trace, label, clean_snr, desired_snr, noise_trace, calc_snr, p
             fix_step /= (-2.0)
 
     return factor
+
